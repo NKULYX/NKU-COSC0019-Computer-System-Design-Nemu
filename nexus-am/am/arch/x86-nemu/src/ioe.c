441a84e1389a10398fcc52f7ac0a4945f8a33a82
tracer-ics2017 <tracer@njuics.org>
2023-04-17 22:11:12 -0700

    >  run 2012677 ubuntu Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  22:11:12 up 9 min,  1 user,  load average: 0.66, 0.73, 0.47 6917a8c3ee296562bdf113ac54ce2dc885f56be

--- nexus-am/am/arch/x86-nemu/src/ioe.c
+++ nexus-am/am/arch/x86-nemu/src/ioe.c
@@ -23,8 +23,9 @@ extern void* memcpy(void *, const void *, int);
 
 void _draw_rect(const uint32_t *pixels, int x, int y, int w, int h) {
   int i;
-  for(i=0;i<h;i++)
-      memcpy(fb+(y+i)*_screen.width+x,pixels+i*w,w*4);
+  for (i = 0; i < _screen.width * _screen.height; i++) {
+    fb[i] = i;
+  }
 }
 
 void _draw_sync() {
