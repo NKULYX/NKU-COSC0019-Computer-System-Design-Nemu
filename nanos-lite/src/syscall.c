7647732b573d35db40373bd553bf023744fa4481
tracer-ics2017 <tracer@njuics.org>
2023-05-05 23:38:40 -0700

    >  run 2012677 ubuntu Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  23:38:40 up  4:09,  1 user,  load average: 0.06, 0.42, 0.45 9b1678fc4751a66f405b696839d29afeed1f2449

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -19,7 +19,6 @@ static inline _RegSet* sys_write(_RegSet *r){
   size_t count = (int)SYSCALL_ARG4(r);
   uintptr_t i = 0;
   if(fd == 1 || fd == 2) {
-    printf("!");
     for(;i < count; i++) {
       _putc(buf[i]);
     }
