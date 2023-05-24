513813ccfd470d91c6ec94e2cf3c7b473dce85cb
tracer-ics2017 <tracer@njuics.org>
2023-05-24 06:17:27 -0700

    >  compile 2012677 ubuntu Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  06:17:27 up 11:33,  1 user,  load average: 1.02, 0.83, 0.82 7715d10033feca0883945201ed8b7eec520ca49e

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -15,7 +15,8 @@ void exec_wrapper(bool);
 
 /* Simulate how the CPU works. */
 void cpu_exec(uint64_t n) {
-  if(cpu.eip == 0x10129b) {
+  printf("eip: 0x%x\n", cpu.eip);
+  if(cpu.eip == 0x0010129b) {
     printf("0x%x\n", cpu.CR0);
     printf("0x%x\n", cpu.CR3);
     assert(0);
