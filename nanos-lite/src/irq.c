e40a32d3e37c10c56569ed393203c7d588ed8cd5
tracer-ics2017 <tracer@njuics.org>
2023-05-27 09:13:38 -0700

    >  compile 2012077 ubuntu Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  09:13:38 up 18:50,  1 user,  load average: 1.00, 0.81, 0.78 cabe9d2e85cfefba983f8ef145ed8e56e385c32c

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -5,6 +5,7 @@ _RegSet* do_syscall(_RegSet *r);
 static _RegSet* do_event(_Event e, _RegSet* r) {
   switch (e.event) {
     case _EVENT_SYSCALL: return do_syscall(r);
+    case _EVENT_TRAP: printf("Self Trap!!!\n"); return NULL;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
